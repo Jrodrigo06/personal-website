ad8578fc8ef6b91541fb4036aaee8e72b87fc7eb
Jrodrigo06 <jeromerodrigo06@gmail.com>
2025-05-21 10:55:17 -0700

    Added star background

--- personal-website/src/pages/Home.tsx
+++ personal-website/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { ThemeToggle } from "../components/ThemeToggle";
 import {StarBackground} from '../components/StarBackground'
+
 export const Home = () => {
 
     return <div className = "min-h-dvh bg-background text-foreground overflow-x-hidden">
