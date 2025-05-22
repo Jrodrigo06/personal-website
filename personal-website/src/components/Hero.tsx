a9f13888b5c9f6015baebeb94e1616cbf23a4c71
Jrodrigo06 <jeromerodrigo06@gmail.com>
2025-05-22 02:18:22 -0700

    added hover cards

--- personal-website/src/components/Hero.tsx
+++ personal-website/src/components/Hero.tsx
@@ -6,11 +6,67 @@ export const Hero = () => {
     className="relative min-h-dvh flex flex-col items-center justify-center px-4"
     >
         <div className="container max-w-4xl max-auto text-center z-10">
-        <img
-        src={ProfilePic}
-        alt="Jerome Rodrigo"
-        className="w-45 h-45 md:w-48 md:h-48 rounded-full border-4 border-background shadow-lg object-cover mx-auto"
-            />
+        
+        {/* Profile Picture */}
+        <div className="relative inline-block group p-3">
+            <img
+            src={ProfilePic}
+            alt="Jerome Rodrigo"
+            className="w-[32vh] h-[32vh] rounded-full border-4 border-background shadow-lg object-cover mx-auto"
+                />
+
+            {/* Left Card */}
+            <div
+                className="
+                absolute left-0 top-1/2 -translate-y-1/2
+                w-[70v] p-3 bg-background border border-white/50 rounded-lg
+                opacity-0 -translate-x-[1rem]
+                group-hover:opacity-100 group-hover:-translate-x-[250px]
+                transition-all duration-1000
+                whitespace-normal md:whitespace-nowrap
+                "
+            >
+                <div className="space-y-1 text-sm">
+                    <p>
+                        <span className="font-semibold">School:</span> Northeastern University
+                    </p>
+                    <p>
+                        <span className="font-semibold">Major:</span> Data Science and Math
+                    </p>
+                    <p>
+                        <span className="font-semibold">Graduation Date:</span> May '28
+                    </p>
+                </div>
+            </div>
+
+            {/* Right Card */}
+            <div
+                className="
+                absolute right-0 top-1/2 -translate-y-1/2
+                w-[70v] p-3 bg-background border border-white/50 rounded-lg
+                opacity-0 translate-x-[1rem]
+                group-hover:opacity-100 group-hover:translate-x-[250px]
+                transition-all duration-1000
+                whitespace-normal md:whitespace-nowrap
+                "
+            >
+            <div className="space-y-1 text-sm">
+                    <p>
+                        <span className="font-semibold">From:</span> Albany, NY
+                    </p>
+                    <p>
+                        <span className="font-semibold">Hobbies:</span> Cooking, Photography
+                    </p>
+                    <p>
+                        <span className="font-semibold">Favorite Food:</span> Quessadilla
+                    </p>
+                </div>
+        </div>
+        </div>
+            
+
+            {/**                   */}            
+
             <div className="space-y-6">
                 
                 <h1 className="text-4xl md:6xl font-bold tracking-tight">
@@ -18,7 +74,7 @@ export const Hero = () => {
                 <span className="text-primary opacity-0 animate-fade-in-delay-1"> 
                     {" "}
                     Jerome</span>
-                <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
+                <span className="text-gradient opacity-0 animate-fade-in-delay-2">
                     {" "}
                      Rodrigo
                      </span>
