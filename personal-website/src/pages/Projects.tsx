243c23612e9e6e2d62aa991d6c0dc538cca71e1a
Jrodrigo06 <jeromerodrigo06@gmail.com>
2025-05-26 12:35:49 -0700

    Worked more on projects page

--- personal-website/src/pages/Projects.tsx
+++ personal-website/src/pages/Projects.tsx
@@ -1,3 +1,4 @@
+import { Github } from "lucide-react"
 import { ThemeToggle } from "../components/ThemeToggle"
 
 const projects = [
@@ -47,10 +48,29 @@ export const Projects = () => {
                 </p>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8"> 
-                    {projects.map((projects, key) => (
-                        <div key={key} className='group bg-card rounded-lg overflow-hidden shadow-xs card-hover'>  
-                            <div>
-                                
+                    {projects.map((project, key) => (
+                        <div key={key} className='group text-center bg-card rounded-lg overflow-hidden shadow-xs card-hover'>  
+                            <div className="p-5">
+                            <h3 className="text-xl font-semibold mb-3 hover:text-primary transition-colors duration-300"> {project.title} </h3>
+                                <div className="flex flex-wrap gap-2 mb-4 items-center"> 
+                                    {project.tags.map((tag) => (
+                                        <span className="px-2 py-1 text-xs font-medium rounded-full border bg-primary/20 text-secondary-foreground">{tag}</span>
+                                    ))}
+
+                                   
+                                    <p className="text-left text-muted-foreground text-sm mb-4">{project.description}</p>
+
+                                    <div className="flex justify-between items-center"> 
+                                        <div className="flex space-x-3">
+                                            <a 
+                                            href={project.githubUrl} 
+                                            target="_blank"
+                                            className="text-foreground/80 hover:text-primary transition-colors duration-300">
+                                                <Github size={20}/>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>    
                     ))}
