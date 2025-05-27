ed63d7f7d8e7a0427354f4161bc0293ae8e16e2f
Jrodrigo06 <jeromerodrigo06@gmail.com>
2025-05-27 01:24:34 -0700

    Fixed navbar scroll for windows

--- personal-website/src/components/Navbar.tsx
+++ personal-website/src/components/Navbar.tsx
@@ -21,7 +21,7 @@ export const Navbar = () => {
 
     useEffect(() => {
         const handleScroll = () => {
-          setIsScrolled(window.screenY > 10);
+          setIsScrolled(window.scrollY > 10);
         };
     
         window.addEventListener("scroll", handleScroll);
