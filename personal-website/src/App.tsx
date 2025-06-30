ccd8c9722151ebebced657d3ba0a100ac524ffff
Jrodrigo06 <jeromerodrigo06@gmail.com>
2025-06-30 10:54:24 -0700

    Added pdfs

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -1,6 +1,5 @@
 // import { useState } from 'react'
-import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom"
-import { useEffect } from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 import { Home } from "./pages/Home";
 import {NotFound} from "./pages/NotFound"
 import { Navbar } from "./components/Navbar";
@@ -12,14 +11,6 @@ import { Photos } from "./pages/Photos";
 
 function App() {
 
-  function BlackJackRedirect() {
-    useEffect(() => {
-      // this is a real browser navigation, not just React Router
-      window.location.href = "/BlackJackRL.pdf";
-    }, []);
-  
-    return null;
-  }
 
 
   return (
